9bcc875da99a94c32bf6cf08b6e465523f5f3aef
Stoneroadd <master.robless@gmail.com>
2026-08-06 13:12:25 -0400

    feat(landing): money truck running loader like Chrome dino

---
+++ frontend/src/components/brand/LoadingScene.tsx
@@ -0,0 +1,13 @@
+export function LoadingScene() {
+  return (
+    <main className="nm-splash-scene">
+      <div className="nm-splash-copy">
+        Cargando dinero<span className="nm-splash-dots" />
+      </div>
+      <div className="nm-splash-road" />
+      <div className="nm-splash-truck">
+        <img src="/assets/brand/loading-truck.svg" alt="Camión cargando dinero" />
+      </div>
+    </main>
+  )
+}
